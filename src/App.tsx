d24e6298de9acd8992fab9705291a16ce4e6da9d
Bailey Nield <bailey.nield18@gmail.com>
2024-07-17 11:11:39 -0600

    Initial Commmit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>ETA Calculator</h1>
+        <p>Given distance to destination and speed, this app will return the time to destination</p>
+      </header>
+      <main>
+        <></>
+      </main>
+    </>
+  );
+}
+
+export default App
